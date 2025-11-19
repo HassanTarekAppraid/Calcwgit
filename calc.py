b93d593784b31c2379b5f6ee486c183a57beaf86
Halla Hesham <EXT.Halla.Hesham@appraid-tech.com>
2025-11-19 14:02:04 +0200

    updated

--- calc.py
+++ calc.py
@@ -8,18 +8,20 @@ def subtract(num1, num2):
 #print(subtract(4, 2))
 
 print("Welcome to H&H Git Practice Calculator :)\n"
-"Press - for Subtraction\n")
+"Press - for Subtraction & + for Addition\n")
 
-selection = input("Select Operation (-): ")
+selection = input("Select Operation (-,+): ")
 num1 = float(input("Enter the First Number: "))
 num2 = float(input("Enter the Second Number: "))
 
 if selection == '-':
     result=subtract(num1, num2)
     print(num1,"-",num2, "=", result)
-elseif selection == '+' :
+elif selection == '+' :
     result=add(num1, num2)
-    print(num1,"-",num2, "=", result)   
+    print(num1,"+",num2, "=", result)
+else:
+    print("this is an invalid operation")       
 
 
 
