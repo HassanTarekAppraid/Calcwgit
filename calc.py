6d22c493acd6f03ec130d82b3b8e879fec65e614
Halla Hesham <EXT.Halla.Hesham@appraid-tech.com>
2025-11-19 16:55:20 +0200

    added comments

--- calc.py
+++ calc.py
@@ -1,27 +1,27 @@
-def add(num1, num2):
+def add(num1, num2): #function to add numbers
     return num1 + num2
 
-def subtract(num1, num2):
+def subtract(num1, num2): #function to subtract numbers
     return num1-num2
 
 #print(add(4, 2))
 #print(subtract(4, 2))
 
 print("Welcome to H&H Git Practice Calculator :)\n"
-"Press - for Subtraction & + for Addition\n")
+"Press - for Subtraction & + for Addition\n") #welcome message
 
-selection = input("Select Operation (-,+): ")
-num1 = float(input("Enter the First Number: "))
-num2 = float(input("Enter the Second Number: "))
+selection = input("Select Operation (-,+): ") #asks user to select operation
+num1 = float(input("Enter the First Number: ")) #asks user to enter first number
+num2 = float(input("Enter the Second Number: ")) #asks user to enter second number
 
-if selection == '-':
+if selection == '-': #if condition for subtraction feature
     result=subtract(num1, num2)
-    print(num1,"-",num2, "=", result)
-elif selection == '+' :
+    print(num1,"-",num2, "=", result) #result
+elif selection == '+' : #condition for addition feature
     result=add(num1, num2)
-    print(num1,"+",num2, "=", result)
+    print(num1,"+",num2, "=", result) #result
 else:
-    print("this is an invalid operation")       
+    print("this is an invalid operation")   #if none of the operations changed    
 
 
 
