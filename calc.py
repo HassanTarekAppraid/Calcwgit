7be20ae0e73c85fe737596a6b3f0ef6f5c38efd6
Hassan <EXT.Hassan.Tarek@appraid-tech.com>
2025-11-19 16:55:45 +0200

    Comments On the Code

--- calc.py
+++ calc.py
@@ -1,19 +1,25 @@
+#Addition Main Function
 def add(num1, num2):
     return num1 + num2
-
+#Subtract Main Function
 def subtract(num1, num2):
     return num1-num2
 
 #print(add(4, 2))
 #print(subtract(4, 2))
 
+#Welcome Message
 print("Welcome to H&H Git Practice Calculator :)\n"
 "Press - for Subtraction & + for Addition\n")
 
+#User select which operation to make
 selection = input("Select Operation (-,+): ")
+
+#Getting the numbers from the user
 num1 = float(input("Enter the First Number: "))
 num2 = float(input("Enter the Second Number: "))
 
+#If condition based on user selection
 if selection == '-':
     result=subtract(num1, num2)
     print(num1,"-",num2, "=", result)
